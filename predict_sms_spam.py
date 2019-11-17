dc3bcc861849f4613297742ad8b3bfb888072563
advu <advu@advus-air.home>
2019-11-17 16:49:01 -0500

    found error and switched dhl_list with dhsw_list in main(). Added revised tree view screenshot and if/else rules

--- predict_sms_spam.py
+++ predict_sms_spam.py
@@ -2,7 +2,7 @@ import csv
 
 # returns new csv file with first row written out
 def write_file():
-    new_file = open("new_spam.csv", "w+")
+    new_file = open("sms_spam_detection.csv", "w+")
     # initialize first row headers
     columns = ["doesHaveLinks", "doesHaveSpammyWords", "LengthOfText", "NumberOfSymbols", "class"]
     i = 0
@@ -14,11 +14,11 @@ def write_file():
     return new_file
 
 # returns the new csv file with the new features
-def add_features_to_file(file, class_list, dsl_list, dhsw_list, lot_list, nos_list, file_length):
+def add_features_to_file(file, class_list, dhl_list, dhsw_list, lot_list, nos_list, file_length):
     # start from the second row to avoid "v1" and "v2" becoming data points
     i = 1
     while i < file_length:
-        file.write(str(dsl_list[i]))
+        file.write(str(dhl_list[i]))
         file.write(",")
         file.write(str(dhsw_list[i]))
         file.write(",")
@@ -30,12 +30,14 @@ def add_features_to_file(file, class_list, dsl_list, dhsw_list, lot_list, nos_li
         file.write("\n")
         i += 1
 
+# returns True if text message has links and False otherwise
 def does_have_links(text_message):
     if "http" in text_message or ".com" in text_message or ".co.uk" in text_message or "www" in text_message:
         return True
     else:
         return False
 
+# returns True if text message has spammy words and False otherwise
 def does_have_spammy_words(text_message):
     if "congratulations!" in text_message or "Â£" in text_message or "private!" in text_message or "std" in text_message or "FREE" in text_message or "4 info" in text_message or "POBOX" in text_message or "prize" in text_message or "rate call" in text_message or "to opt out" in text_message or "years or over" in text_message or "urgent" in text_message:
         return True
@@ -62,27 +64,27 @@ def predict_spam(text_message):
     dhsw = does_have_spammy_words(text_message)
     nos = get_number_of_symbols(text_message)
     lot = get_length(text_message)
-    if dhl == False and lot <= 99 and dhsw == False:
+    if dhsw == False and lot <= 99 and dhl == False:
         return "ham"
-    elif dhl == False and lot <= 99 and dhsw == True and nos <= 4:
+    elif dhsw == False and lot <= 99 and dhl == True and nos <= 4:
         return "ham"
-    elif dhl == False and lot <= 99 and dhsw == True and nos > 4:
+    elif dhsw == False and lot <= 99 and dhl == True and nos > 4:
         return "spam"
-    elif dhl == False and lot > 99 and dhsw == False and lot <= 131:
+    elif dhsw == False and lot > 99 and dhl == False and lot <= 131:
         return "ham"
-    elif dhl == False and dhsw == False and 131 < lot <= 160 and nos <= 8:
+    elif dhsw == False and dhl == False and 131 < lot <= 160 and nos <= 8:
         return "ham"
-    elif dhl == False and dhsw == False and 131 < lot <= 160 and 8 < nos <= 9:
+    elif dhsw == False and dhl == False and 131 < lot <= 160 and 8 < nos <= 9:
         return "spam"
-    elif dhl == False and dhsw == False and 131 < lot <= 160 and 9 < nos <= 14:
+    elif dhsw == False and dhl == False and 131 < lot <= 160 and 9 < nos <= 14:
         return "ham"
-    elif dhl == False and dhsw == False and nos <= 14 and 160 < lot <= 164:
+    elif dhsw == False and dhl == False and nos <= 14 and 160 < lot <= 164:
         return "spam"
-    elif dhl == False and dhsw == False and nos <= 14 and lot > 164:
+    elif dhsw == False and dhl == False and nos <= 14 and lot > 164:
         return "ham"
-    elif dhl == False and dhsw == False and nos > 14 and lot > 131:
+    elif dhsw == False and dhl == False and nos > 14 and lot > 131:
         return "ham"
-    elif dhl == False and dhsw == True:
+    elif dhsw == False and dhl == True and lot > 99:
         return "spam"
     else:
         return "spam"
@@ -93,19 +95,22 @@ def main():
     with open('spam.csv', encoding="ISO-8859-1") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         class_list = []
-        dsl_list = []
+        dhl_list = []
         dhsw_list = []
         lot_list = []
         nos_list = []
         new_file = write_file()
         new_file_length = 0
         for row in csv_reader:
+            # used to create weka decision tree
+            class_list.append(row[classCol])
+            # use to predict spam
             class_list.append(predict_spam(row[messageCol]))
+            dhl_list.append(does_have_links(row[messageCol]))
+            dhsw_list.append(does_have_spammy_words(row[messageCol]))
             lot_list.append(get_length(row[messageCol]))
             nos_list.append(get_number_of_symbols(row[messageCol]))
-            dhsw_list.append(does_have_links(row[messageCol]))
-            dsl_list.append(does_have_spammy_words(row[messageCol]))
             new_file_length += 1
-        add_features_to_file(new_file, class_list, dsl_list, dhsw_list, lot_list, nos_list, new_file_length)
+        add_features_to_file(new_file, class_list, dhl_list, dhsw_list, lot_list, nos_list, new_file_length)
 
 main()
