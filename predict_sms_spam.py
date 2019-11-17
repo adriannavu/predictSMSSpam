9d95b2ad773e7f2c5257ea8b6febd18ceb6566cd
advu <advu@advus-air.home>
2019-11-16 21:16:25 -0500

    new_spam with all features added, replaced commas with ~, 5573 rows exist

--- predict_sms_spam.py
+++ predict_sms_spam.py
@@ -1,9 +1,11 @@
 import csv
 import itertools
 
-def write_out_file():
-    new_file = open("new_spam.csv", "w+")
-    columns = ["class", "message", "doesHaveLinks", "doesHaveSpammyWords", "LengthOfText", "NumberOfSymbols"]
+# returns new csv file with first row written out
+def write_file():
+    new_file = open("new_spam.csv", "w")
+    # initialize first row headers
+    columns = ["class", "doesHaveLinks", "doesHaveSpammyWords", "LengthOfText", "NumberOfSymbols", "message"]
     i = 0
     while i != len(columns):
         new_file.write(columns[i])
@@ -12,11 +14,30 @@ def write_out_file():
     new_file.write("\n")
     return new_file
 
-def add_features_to_file(file, class_list, message_list, dsl_list, dhsw_list, lot_list, nos_list):
-    with open("new_spam.csv", encoding="ISO-8859-1") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        file.write(class_list)
+# returns the new csv file with the new features
+def add_features_to_file(file, class_list, message_list, dsl_list, dhsw_list, lot_list, nos_list, file_length):
+    # if message contains comma, replace it with ~
+    for message in message_list:
+        message.replace(",", "~")
+    # start from the second row to avoid "v1" and "v2" becoming data points
+    i = 1
+    while i < file_length:
+        file.write(class_list[i])
+        file.write(",")
+        file.write(str(dsl_list[i]))
+        file.write(",")
+        file.write(str(dhsw_list[i]))
+        file.write(",")
+        file.write(str(lot_list[i]))
+        file.write(",")
+        file.write(str(nos_list[i]))
+        file.write(",")
+        file.write(str(message_list[i]))
+        file.write("\n")
+        i += 1
+    # if message contains ~, replace it with comma
+    for char in file:
+        char.replace("~", ",")
 
 def does_have_links(text_message):
     if "http" in text_message or ".com" in text_message or ".co.uk" in text_message or "www" in text_message:
@@ -30,15 +51,18 @@ def does_have_spammy_words(text_message):
     else:
         return False
 
-def get_length_of_text(text_message):
+# returns the text length in the text message
+def get_length(text_message):
     return len(text_message)
 
+# returns the number of symbols in the text message
 def get_number_of_symbols(text_message):
     if text_message.isalnum():
         return False
     else:
         return True
 
+# returns "spam" if spam or "ham" if not spam
 def predict_spam(text_message):
     dhl = does_have_links(text_message)
     dhsw = does_have_spammy_words(text_message)
@@ -50,7 +74,7 @@ def predict_spam(text_message):
 def main():
     classCol = 0
     messageCol = 1
-    with open('spam_test.csv', encoding="ISO-8859-1") as csv_file:
+    with open('spam.csv', encoding="ISO-8859-1") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         message_list = []
         class_list = []
@@ -58,23 +82,16 @@ def main():
         dhsw_list = []
         lot_list = []
         nos_list = []
-        #write out file
-        new_file = write_out_file()
-        # for each text message
+        new_file = write_file()
+        in_file_length = 0
         for row in csv_reader:
-            #returns "spam" if spam or "ham" if not spam
             class_list.append(predict_spam(row[messageCol]))
-            #returns the text length in the text message
-            lot_list.append(get_length_of_text(row[messageCol]))
-	        #returns the number of symbols in the text message
+            lot_list.append(get_length(row[messageCol]))
             nos_list.append(get_number_of_symbols(row[messageCol]))
             dhsw_list.append(does_have_links(row[messageCol]))
             dsl_list.append(does_have_spammy_words(row[messageCol]))
             message_list.append(row[messageCol])
-        #write_out_file() and append the features above to the new csv file
-        add_features_to_file(new_file, class_list, message_list, dsl_list, dhsw_list, lot_list, nos_list)
-
-
-
+            in_file_length += 1
+        add_features_to_file(new_file, class_list, message_list, dsl_list, dhsw_list, lot_list, nos_list, in_file_length)
 
 main()
